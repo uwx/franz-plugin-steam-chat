501b337184439a40128e48e2e5517d8f1de29653
uwx <cadrekucra@gmx.com>
2018-06-02 21:02:19 -0300

    add scriptycord

--- webview.js
+++ webview.js
@@ -1,26 +1,26 @@
 module.exports = (Franz) => {
+  // from https://github.com/kevinoes/franz-plugin-steam-chat/blob/master/webview.js
   const getMessages = function getMessages() {
-
-    // get new msg count
+    //// get new msg count
     let count = 0;
     let counters = document.querySelectorAll('.unread_message_count:not([style="display: none;"])');
     counters = Array.prototype.slice.call(counters, Math.floor(counters.length / 2));
-    [].filter.call(counters, function(countValues) {
-      const countValue = countValues.querySelector('.unread_message_count_value');
-    	if (countValue.innerHTML.length > 0) {
-        count += parseInt(countValue.innerHTML);
+    [].filter.call(counters, countValues => {
+      const countValue = countValues.getElementsByClassName('unread_message_count_value')[0];
+      if (countValue && countValue.firstChild && countValue.firstChild.nodeValue) {
+        count += parseInt(countValue.firstChild.nodeValue);
       }
     });
     Franz.setBadge(count);
 
     // force scroll to bottom of chat window
-    const chatBoxes = document.querySelectorAll('.chat_dialog');
-    const chatBox = [].filter.call(chatBoxes, function(chat) {
-    	return (chat.style.display !== 'none');
-    });
-    const chatWindow = chatBox[0].querySelector('.chat_dialog_scroll');
-    chatWindow.scrollTop = chatWindow.scrollHeight;
+    const chatBoxes = document.getElementsByClassName('chat_dialog');
+    const chatBox = Array.prototype.filter.call(chatBoxes, chat => chat.style.display !== 'none');
+    if (chatBox[0]) {
+      const chatWindow = chatBox[0].querySelector('chat_dialog_scroll');
+      chatWindow.scrollTop = chatWindow.scrollHeight;
+    }
   };
-
+  
   Franz.loop(getMessages);
 };
